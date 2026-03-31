dacabc5c3b42122a36a626eb65f9a3003f444b3a
Danc690 <danielcamiloced@gmail.com>
2026-03-31 12:25:28 -0500

    Se llama al método en el main creado anteriormente en la clase DatabaseConnection para inicializar las tablas al iniciar la partida.

--- marrakech-arquitectura/src/main/java/com/marrakech/game/presentation/Main.java
+++ marrakech-arquitectura/src/main/java/com/marrakech/game/presentation/Main.java
@@ -16,5 +16,8 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
-    public static void main(String[] args) { launch(); }
+    public static void main(String[] args){
+        DatabaseConnection.initDatabase();
+        launch();
+    }
 }
